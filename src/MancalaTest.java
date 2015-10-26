e1a1d577e8809cba4824bf3b1c11f7ce5acbb6e1
Scot Matson <sjpmatson@gmail.com>
2015-10-26 11:20:38 -0700

    Developed MancalaTest class which holds the main() method and added STYLEGUIDE documentation in the main directory for convenient referencing.

---
+++ src/MancalaTest.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+
+/**
+
+ COPYRIGHT (C) 2015 Team Tertiary. All Rights Reserved.
+
+ Classes to manipulate widgets.
+
+ Solves CS151 Project - Mancala Game
+
+ @author Kenneth Cross, Scot Matson, Alex Preston
+
+ @version 1.01 2015/10/26
+
+ */
+public class MancalaTest
+{
+   /**
+    Constructor method for the MancalaTest class.
+    */
+   public MancalaTest()
+   {
+      // TODO: Entry point for Mancala Application
+   }
+
+   /**
+    The main method of execution for the JVM.
+    @param args user-defined arguments.
+    */
+   public static void main(String[] args)
+   {
+      SwingUtilities.invokeLater(new Runnable()
+      {
+         @Override
+         public void run()
+         {
+            new MancalaTest();
+         }
+      });
+   }
+}

35572aa2ab9230146e871d15327891de9a2dca5b
Scot Matson <sjpmatson@gmail.com>
2015-11-29 11:42:54 -0800

    Removed the MancalaGUI class.

--- src/MancalaTest.java
+++ src/MancalaTest.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
 
  COPYRIGHT (C) 2015 Team Tertiary. All Rights Reserved.
 
- Classes to manipulate widgets.
+ Class to launch Mancala, include the main() method
 
  Solves CS151 Project - Mancala Game
 
@@ -13,19 +14,37 @@ import javax.swing.*;
  @version 1.01 2015/10/26
 
  */
-public class MancalaTest
+public class MancalaTest extends JFrame
 {
+
+   private final String FRAME_TITLE = "This is Mancala, bitches.";
+
    /**
     Constructor method for the MancalaTest class.
     */
    public MancalaTest()
    {
+      // Define properties of the JFrame.
+      setTitle(FRAME_TITLE);
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+      setDefaultLookAndFeelDecorated(true);
+      MancalaMenuBar menuBar = new MancalaMenuBar();
+
+      // The model
       MancalaModel model = new MancalaModel();
 
+      // Controllers
+      MancalaMenuBarController menuBarController = new MancalaMenuBarController(menuBar, model);
 
+      // Add JPanes to the JFrame
+      Container pane = getContentPane();
+      pane.add(new StatusPane(1000, 150), BorderLayout.PAGE_START);
+      pane.add(new GamePane(1000, 400), BorderLayout.PAGE_END);
 
-
-      new MancalaGUI();
+      // Display the window.
+      setJMenuBar(menuBar);
+      pack();
+      setVisible(true);
    }
 
    /**
@@ -37,13 +56,6 @@ public class MancalaTest
       MancalaModel testModel = new MancalaModel();
       testModel.printCurrentState();
 
-      SwingUtilities.invokeLater(new Runnable()
-      {
-         @Override
-         public void run()
-         {
-            new MancalaTest();
-         }
-      });
+      SwingUtilities.invokeLater(MancalaTest::new);
    }
 }

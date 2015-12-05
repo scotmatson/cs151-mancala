6ec09f0fec7b3d014d8ff413e985d2556536ed1d
k-cross <kenneth.cross@sjsu.edu>
2015-12-05 00:34:43 -0800

    reducing complexity: removing unused functions/classes

--- src/Stone.java
+++ src/Stone.java
@@ -12,45 +12,17 @@ import java.util.Observer;
  * @author Kenneth Cross, Scot Matson, Alex Preston
  * @version 1.01 2015/10/26
  */
-public class Stone extends GameObject implements Observer {
-    private final int STONE_ID;
+public class Stone {
     private final int STONE_WIDTH = 10;
     private final int STONE_HEIGHT = 10;
-    private int playerId;
     private int xPos;
     private int yPos;
-    private boolean inUse; // Can only be added to a cup when false
     private Color color;
 
     /**
      * Constructor method for the Stone class
      */
     public Stone() {
-        STONE_ID = super.getCount();
-        inUse = false;
-    }
-
-    /**
-     * Returns the Stone Id when called
-     */
-    public int getStone() {
-        return STONE_ID;
-    }
-
-    /**
-     * Sets the players Id
-     * @param id
-     */
-    public void setPlayerId(int id) {
-        this.playerId = id;
-    }
-
-    /**
-     * This returns the playerId
-     * @return
-     */
-    public int getPlayerId() {
-        return playerId;
     }
 
     /**
@@ -69,10 +41,5 @@ public class Stone extends GameObject implements Observer {
      */
     public void setColor(Color c) {
         color = c;
-    }
-
-    @Override
-    public void update(Observable o, Object arg) {
-
     }
 }
